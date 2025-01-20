b4403c63513ebf2a6aaba4c869a0b673f7cc655d
Aaliyah Pirani <Aaliyah@cadenceitsolutions.ca>
2025-01-20 10:09:30 -0500

    new icon

--- app/layout.js
+++ app/layout.js
@@ -16,10 +16,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className='scroll-smooth'>
+    <html lang="en" className="scroll-smooth">
+      <head>
+        {/* Intentionally exclude the favicon link */}
+      </head>
       <body
-        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme
-        dark:text-white`}
+        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
       >
         {children}
       </body>
